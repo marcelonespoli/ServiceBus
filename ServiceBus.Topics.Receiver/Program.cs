5ceecd1ac2c36998bd478b8871177d60d3a0bf51
Marcelo <marcelonespoli@outlook.com>
2024-01-28 16:31:20 +0000

    Added senders and receivers

---
+++ ServiceBus.Topics.Receiver/Program.cs
@@ -0,0 +1,47 @@
+﻿using Azure.Messaging.ServiceBus;
+
+const string ServiceConnectionString = "Endpoint=sb://az-course-mns.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=zLUh1iLTzzyfRnIQ/tGLOWWzcPnAIjzpc+ASbLp7gas=";
+//const string QueueName = "high-priority";
+const string TopicName = "order-topic";
+const string Subscription1Name = "Sub1";
+
+ServiceBusClient client;
+ServiceBusProcessor processor = default!;
+
+async Task MessageHandler(ProcessMessageEventArgs processMessageEventArgs)
+{
+    string body = processMessageEventArgs.Message.Body.ToString();
+    Console.WriteLine($"{body} - Subscription: {Subscription1Name}");
+    await processMessageEventArgs.CompleteMessageAsync(processMessageEventArgs.Message);
+}
+
+Task ErrorHandler(ProcessErrorEventArgs processMessageEventArgs)
+{
+    Console.WriteLine(processMessageEventArgs.Exception.ToString());
+    return Task.CompletedTask;
+}
+
+client = new ServiceBusClient(ServiceConnectionString);
+//processor = client.CreateProcessor(QueueName, new ServiceBusProcessorOptions());
+processor = client.CreateProcessor(TopicName, Subscription1Name, new ServiceBusProcessorOptions());
+
+try
+{
+    // Everytime this event is fired I want to call the corresponding message handler
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+    Console.WriteLine("Press any key to end teh processing");
+    Console.ReadKey();
+    Console.WriteLine("Stopped receiving message");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Exception: " + ex.Message);
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
---
+++ ServiceBus.Topics.Sender/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using Azure.Messaging.ServiceBus;
+
+const string ServiceConnectionString = "Endpoint=sb://az-course-mns.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=zLUh1iLTzzyfRnIQ/tGLOWWzcPnAIjzpc+ASbLp7gas=";
+//const string QueueName = "high-priority";
+const string TopicName = "order-topic";
+const int MaxNumberOfMessages = 3;
+
+ServiceBusClient client;
+ServiceBusSender sender;
+
+client = new ServiceBusClient(ServiceConnectionString);
+//sender = client.CreateSender(QueueName);
+sender = client.CreateSender(TopicName);
+
+using ServiceBusMessageBatch batch = await sender.CreateMessageBatchAsync();
+for (int i = 1; i <= MaxNumberOfMessages; i++)
+{
+    if (batch.TryAddMessage(new ServiceBusMessage($"This a message - {i}")))
+        Console.WriteLine($"Message - {i} was added to the batch");
+}
+
+try
+{
+    await sender.SendMessagesAsync(batch);
+    Console.WriteLine("Batch of Messages Sent");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Messages not sent, Error: " + ex.Message);
+}
+finally
+{
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+
